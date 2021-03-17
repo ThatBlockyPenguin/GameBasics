8d0ee1b2da546a5ee9d12ce868a859f41358a56f
ThatBlockyPenguin <pixelorangedev@gmail.com>
2021-03-17 09:57:39 +0000

    Initial Commit + Release 1.0.0 (LibGDX 1.9.14) :tada:

---
+++ core/src/main/java/com/blockypenguin/gamebasicslib/Events.java
@@ -0,0 +1,18 @@
+package com.blockypenguin.gamebasicslib;
+
+public class Events {
+	public static void sendEvent(GameBase game, EventType type, String... data) {
+		if(type == EventType.SCREEN) game.getScreen().ifPresent(s -> s.receiveEvent(data));
+		if(type == EventType.GAME) game.receiveEvent(data);
+		if(type == EventType.BOTH) {
+			game.receiveEvent(data);
+			game.getScreen().ifPresent(s -> s.receiveEvent(data));
+		}
+	}
+	
+	public static enum EventType {
+		SCREEN,
+		GAME,
+		BOTH
+	}
+}
---
+++ core/src/main/java/com/blockypenguin/gamebasicslib/Quadruple.java
@@ -0,0 +1,36 @@
+package com.blockypenguin.gamebasicslib;
+
+public class Quadruple<T1, T2, T3, T4> {
+	
+	private final T1 t1;
+	private final T2 t2;
+	private final T3 t3;
+	private final T4 t4;
+	
+	private Quadruple(T1 t1, T2 t2, T3 t3, T4 t4) {
+		this.t1 = t1;
+		this.t2 = t2;
+		this.t3 = t3;
+		this.t4 = t4;
+	}
+	
+	public static <T1, T2, T3, T4> Quadruple<T1, T2, T3, T4> of(T1 t1, T2 t2, T3 t3, T4 t4) {
+		return new Quadruple<T1, T2, T3, T4>(t1, t2, t3, t4);
+	}
+	
+	public T1 getT1() {
+		return t1;
+	}
+	
+	public T2 getT2() {
+		return t2;
+	}
+	
+	public T3 getT3() {
+		return t3;
+	}
+	
+	public T4 getT4() {
+		return t4;
+	}
+}
